fc45ea3febc9f5bf5c7b9405a2444a2ae204e0ce
JessieSu2 <JessieSu3@gmail.com>
2023-02-26 22:01:34 -0500

    added animations

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -208,42 +208,44 @@ function HomePage() {
 
       </Parallax> */}
       <div className="background">
-        
+        <div className="background-image">
           <div id="me">
-             <NavBar />
+            <NavBar />
             <div className="container">
               <div id="intro-name">Hi, I'm</div>
               <h1 id="name">Jessie Su </h1>
               <h2 id="title">Junior Software Engineer</h2>
               <div id="icons">
-                <div className="icon-spacing">
+                <div className="icon-spacing bob">
                   <a href="https://github.com/JessieSu2" target="_blank" rel="noopener noreferrer">
-                    <Icon icon="uit:github-alt" color="white" width="48" height="48" alt="GitHub: JessieSu2"/>
+                    <Icon icon="uit:github-alt" alt="GitHub: JessieSu2" className="intro-icons bob"/>
                   </a>
                 </div>
                 <div className="icon-spacing">
                   <a href="https://www.linkedin.com/in/jessiesu01/" target="_blank" rel="noopener noreferrer">
-                    <Icon icon="bi:linkedin" color="white" width="48" height="48" alt="LinkedIn: Jessiesu01"/>
+                    <Icon icon="bi:linkedin"  alt="LinkedIn: Jessiesu01" className="intro-icons bob"/>
                   </a>
                 </div>
                 <div className="icon-spacing">
                   <a href="mailto:jessiesu3@gmail.com">
-                    <Icon icon="octicon:mail-24" color="white" width="48" height="48" alt="Email: JessieSu3@gmail.com"/>
+                    <Icon icon="octicon:mail-24"  alt="Email: JessieSu3@gmail.com" className="intro-icons bob"/>
                   </a>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="what-do mx-auto">
-          I've always been interested in the vast capabilities of technology, whether it be gaming, accessibility, or innovation. 
-          Games can teach people about mental health and problems in the world or simply for enjoyment. 
-          I love that technology can make people's lives easier through software or engineering. 
-          I want to create products or services that help people, especially the elders, where these things are not as accessible. 
-          Creating something that no one thought about before or something that will be the next big thing is fascinating. 
-          One small idea can turn into something big!
-
+          <div className="what-do-container">
+             <div className="what-do mx-auto">
+              I've always been interested in the vast capabilities of technology, whether it be gaming, accessibility, or innovation. 
+              Games can teach people about mental health and problems in the world or simply for enjoyment. 
+              I love that technology can make people's lives easier through software or engineering. 
+              I want to create products or services that help people, especially the elders, where these things are not as accessible. 
+              Creating something that no one thought about before or something that will be the next big thing is fascinating. 
+              One small idea can turn into something big!
+            </div>
           </div>
+           
 
           <div className="projects">
             <h1 className="project-title-section mx-auto">Latest Projects</h1>
@@ -254,7 +256,7 @@ function HomePage() {
                   <div className="title-icon-container">
                     <div className="projects-title">Easy Trader</div>
                     <a href="https://github.com/jingshiliu/easy-trader-ctp" target="_blank" rel="noopener noreferrer" className="project-icon-container">
-                      <Icon icon="uit:github-alt"  className="projects-icon"/>
+                      <Icon icon="uit:github-alt"  className="projects-icon bob" />
                     </a>
                   </div>
                   <div className="project-description">
@@ -274,7 +276,7 @@ function HomePage() {
                   <div className="title-icon-container">
                     <div className="projects-title">One Bite</div>
                     <a href="https://github.com/JessieSu2/CSC322-OneBite" target="_blank" rel="noopener noreferrer" className="project-icon-container">
-                      <Icon icon="uit:github-alt"  className="projects-icon"/>
+                      <Icon icon="uit:github-alt"  className="projects-icon bob"/>
                     </a>
                   </div>
                   <div className="project-description">
@@ -297,10 +299,10 @@ function HomePage() {
                   <div className="title-icon-container">
                     <div className="projects-title">Coffee Run</div>
                     <a href="https://github.com/JessieSu2/CoffeeRun_Game" target="_blank" rel="noopener noreferrer" className="project-icon-container">
-                      <Icon icon="uit:github-alt"  className="projects-icon"/>
+                      <Icon icon="uit:github-alt"  className="projects-icon bob"/>
                     </a>
                     <a href="https://www.youtube.com/watch?v=vSUM_U1Rjfk&ab_channel=Jessie" target="_blank" rel="noopener noreferrer" className="project-icon-container">
-                      <Icon icon="ph:youtube-logo-thin"  className="projects-icon"/>
+                      <Icon icon="ph:youtube-logo-thin"  className="projects-icon bob"/>
                     </a>
                   </div>
                   <div className="project-description">
@@ -318,10 +320,10 @@ function HomePage() {
                   <div className="title-icon-container">
                     <div className="projects-title">Magical Walk</div>
                       <a href="https://github.com/JessieSu2/MagicalWalk" target="_blank" rel="noopener noreferrer" className="project-icon-container">
-                        <Icon icon="uit:github-alt"  className="projects-icon"/>
+                        <Icon icon="uit:github-alt"  className="projects-icon bob"/>
                       </a>
                       <a href="https://www.youtube.com/watch?v=uMG2iKQ3erE&ab_channel=Jessie" target="_blank" rel="noopener noreferrer" className="project-icon-container">
-                        <Icon icon="ph:youtube-logo-thin"  className="projects-icon"/>
+                        <Icon icon="ph:youtube-logo-thin"  className="projects-icon bob"/>
                       </a> 
                   </div>
                   <div className="project-description">
@@ -341,10 +343,10 @@ function HomePage() {
                     <div className="projects-title">Mini-Might</div>  
                     <div className="project-icon-container">
                       <a href="https://github.com/JessieSu2/Mini-Might" target="_blank" rel="noopener noreferrer" className="project-icon-container" >
-                        <Icon icon="uit:github-alt" className="projects-icon"/>
+                        <Icon icon="uit:github-alt" className="projects-icon bob"/>
                       </a>
                       <a href="https://www.youtube.com/watch?v=8y-iWIHSUsE&ab_channel=Jessie" target="_blank" rel="noopener noreferrer" className="project-icon-container">
-                        <Icon icon="ph:youtube-logo-thin" className="projects-icon"/>
+                        <Icon icon="ph:youtube-logo-thin" className="projects-icon bob"/>
                       </a>
                     </div>
                   </div>
@@ -366,7 +368,7 @@ function HomePage() {
                   <div className="title-icon-container">
                     <div className="projects-title">Toodaloo</div>
                       <a href="https://github.com/too-da-loo/toodaloo" target="_blank" rel="noopener noreferrer" className="project-icon-container"> 
-                        <Icon icon="uit:github-alt" className="projects-icon"/>
+                        <Icon icon="uit:github-alt" className="projects-icon bob"/>
                       </a>
                   </div>
                   <div className="project-description">
@@ -385,6 +387,7 @@ function HomePage() {
             </ul>
           </div>
 
+        </div>
         </div>
       </div>
       
